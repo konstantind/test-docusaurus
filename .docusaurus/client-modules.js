da765ade0203b606b356c63c85708df86bdd14fc
Konstantin D <merty.doc@gmail.com>
2021-11-19 13:36:12 +0300

    init

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/konstantindubelev/Projects/test-docusaurus/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/konstantindubelev/Projects/test-docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/konstantindubelev/Projects/test-docusaurus/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/konstantindubelev/Projects/test-docusaurus/src/css/custom.css"),
+];
